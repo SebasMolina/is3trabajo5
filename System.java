2372d317bc638f51158b63286ada28d53d0cb4aa
SebasMolina <seba.molina.2013@gmail.com>
2020-05-28 15:05:30 -0300

    operacion resta agregada-Closes #6

--- System.java
+++ System.java
@@ -2,17 +2,18 @@ public class System {
     
     public static void main(String[] args) {
     // TODO code application logic here
-        private int num1;
-        private int num2;
+        private int num1 = 10;
+        private int num2 = 5;
         private int resultado;
         //-----------SUMA---------------//
 
         System.out.println();
 
         //-----------RESTA--------------//
-        
-        System.out.println();
-
+        //se agrega la operacion resta.
+        resultado = num1 - num2;
+        //se imprime el resultado por pantalla.
+        System.out.println(resultado);
         //------------------------------//
 
 
