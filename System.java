f00360faea363bebdd667390e3da4d0ef0050eac
SebasMolina <seba.molina.2013@gmail.com>
2020-05-28 13:34:01 -0300

    se agrego el archivo system.java

---
+++ System.java
@@ -0,0 +1,19 @@
+public class System {
+    
+    public static void main(String[] args) {
+    // TODO code application logic here
+        private int num1;
+        private int num2;
+        private int resultado;
+        //-----------SUMA---------------//
+
+        System.out.println();
+
+        //-----------RESTA--------------//
+        
+        System.out.println();
+
+        //------------------------------//
+
+
+    }
